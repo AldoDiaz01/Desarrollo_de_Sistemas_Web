481a080b39bf0eb8c2465cd92e9d9412eb30218a
dnieln7 <dnieln7@gmail.com>
2020-12-11 11:55:51 -0600

    se agrego api rest

---
+++ Proyecto Final/presi-api/src/main/java/com/empresaurios/presiapi/partido/PartidoRepository.java
@@ -0,0 +1,6 @@
+package com.empresaurios.presiapi.partido;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PartidoRepository extends CrudRepository<Partido, Integer> {
+}
